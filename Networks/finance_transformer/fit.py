361f15100ba56879dd930fbe1bfcddebf61d81a3
KarryRen <KarryRenKai@outlook.com>
2024-07-23 15:31:50 +0800

    build up the fit (#4)

--- Networks/finance_transformer/fit.py
+++ Networks/finance_transformer/fit.py
@@ -6,30 +6,221 @@
 
 import torch
 from torch import nn
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
 
 
 # classes
 
+class FeedForward(nn.Module):
+    """ The FeedForward Module. Just is `2 layers MLP`. """
+
+    def __init__(self, dim: int, hidden_dim: int, dropout: float = 0.):
+        """ Init function of FeedForward.
+
+        :param dim: the dimension of input feature
+        :param hidden_dim: the hidden dimension
+        :param dropout: the dropout ratio
+
+        """
+
+        super().__init__()
+
+        # ---- A 2 layer MLP ---- #
+        self.ff_net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Forward function of FeedForward.
+
+        :param x: the input feature, shape=(bs, n, dim)
+
+        :return: the ff(x), shape=x.shape
+
+        """
+
+        return self.ff_net(x)
+
+
+class MultiHeadAttention(nn.Module):
+    """ The Multi Head Attention Module with the self - `scale dot attention` mechanism. """
+
+    def __init__(self, dim: int, heads: int, dropout: float = 0.):
+        """ Init function of MultiHeadAttention.
+
+        :param dim: the dimension of input feature
+        :param heads: the number of heads
+        :param dropout: the dropout ratio
+
+        """
+
+        super().__init__()
+
+        # ---- Check and set the params --- #
+        assert dim % heads == 0, f"The dim must be divided by heads. Now `dim = {dim}`, `heads = {heads}` !"
+        self.heads = heads
+
+        # ---- The Weight of Q, K, V ---- #
+        self.w_q = nn.Linear(dim, dim, bias=False)
+        self.w_k = nn.Linear(dim, dim, bias=False)
+        self.w_v = nn.Linear(dim, dim, bias=False)
+        self.scale = (dim // heads) ** 0.5
+
+        # ---- The dropout of attention ---- #
+        self.attn_dropout = nn.Dropout(dropout)
+
+        # ---- Output weight ---- #
+        self.w_o = nn.Linear(dim, dim, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Forward function of MultiHeadAttention.
+
+        :param x: the input feature, will do the self multi head attention, shape=(bs, n, dim)
+
+        :return out: feature after attention computing, shape=x.shape
+
+        """
+
+        # ---- Do the QKV weighting ---- #
+        q, k, v = self.w_q(x), self.w_k(x), self.w_v(x)  # shape=(bs, n, dim)
+
+        # ---- Split to multi heads --- #
+        q = rearrange(q, "bs n (h d) -> bs h n d", h=self.heads)  # shape=(bs, heads, n, dim//heads)
+        k = rearrange(k, "bs n (h d) -> bs h n d", h=self.heads)  # shape=(bs, heads, n, dim//heads)
+        v = rearrange(v, "bs n (h d) -> bs h n d", h=self.heads)  # shape=(bs, heads, n, dim//heads)
+
+        # ---- Compute the `attn_score = (qk^T) / (sqrt(d_k <scale>))` ---- #
+        attn_score = torch.matmul(q, k.transpose(-1, -2)) / self.scale  # shape=(bs, heads, n, n)
+
+        # ---- Compute the `attn_weight` and do dropout ---- #
+        attn_weight = nn.functional.softmax(attn_score, dim=-1)  # shape=(bs, heads, n, n)
+        attn_weight = self.attn_dropout(attn_weight)  # shape=(bs, heads, n, n)
+
+        # ---- Compute the `out` ---- #
+        out = torch.matmul(attn_weight, v)  # shape=(bs, heads, n, dim//heads)
+        out = rearrange(out, "bs h n d -> bs n (h d)")  # shape=(bs, n, dim)
+        out = self.w_o(out)
+        return out
+
+
+class Transformer(nn.Module):
+    """ The transformer.  Ref https://arxiv.org/abs/1706.03762 """
+
+    def __init__(self, dim: int, depth: int, heads: int, ff_hidden_dim: int, dropout: float = 0.):
+        """ Init function of Transformer.
+
+        :param dim: the dimension of input feature
+        :param depth: the number of layers
+        :param heads: the number of heads in `multi_attention_head`
+        :param ff_hidden_dim: the hidden dim of FeedForward Module
+        :param dropout: the dropout ratio.
+
+        """
+
+        super().__init__()
+
+        # ---- The core layers ---- #
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                MultiHeadAttention(dim=dim, heads=heads, dropout=dropout),
+                nn.LayerNorm(dim),
+                FeedForward(dim=dim, hidden_dim=ff_hidden_dim, dropout=dropout),
+                nn.LayerNorm(dim)
+            ]))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Forward function of Transformer.
+
+        :param x: input feature, shape=(bs, n, dim)
+
+        :return: out: the encoded feature, shape=(bs, n, dim)
+
+        """
+
+        # ---- Computing layer by layer ---- #
+        for attn, norm_attn, ff, norm_ff in self.layers:
+            x = norm_attn(attn(x) + x)  # Add&Norm shape=(bs, n, dim)
+            x = norm_ff(ff(x) + x)  # Add&Norm shape=(bs, n, dim)
+
+        # ---- Get the output ---- #
+        out = x
+        return out
+
+
 class FiT(nn.Module):
     """ FiT, A new Plug-and-Play module for Finance Cross-Section Feature Extraction. """
 
-    def __init__(self, ):
+    def __init__(
+            self, input_size: int, dim: int, depth: int, heads: int, ff_hidden_dim: int,
+            pool: str = "fin", lp_dropout: float = 0., te_dropout: float = 0.
+    ):
         """ Init function of the FiT.
 
+        :param input_size: the input feature size
+        :param dim: the dimension of feature after Linear Projection(LP) and the feature before the Transformer Encoder (TE)
+        :param depth: the depth of TE, which means the number of transformer encoder layer
+        :param heads: the number of head of TE
+        :param ff_hidden_dim: the hidden dimension of Feed Forward
+        :param pool: the pooling way after TE, you have two choice now:
+            - `fin`: use the feature corresponding to the [fin] token
+            - `mean`: mean all features after TE
+        :param lp_dropout: the dropout ratio of LP
+        :param te_dropout: the dropout ratio of TE
 
         """
 
-        super(FiT, self).__init__()
+        super().__init__()
+        assert pool in ("fin", "mean"), f"`pool` type must be either cls (cls token) or mean (mean pooling), now is {pool}"
+        self.pool = pool
 
-    def forward(self, panel_features: torch.Tensor) -> torch.Tensor:
+        # ---- The linear projection ---- #
+        self.linear_projection = nn.Sequential(
+            nn.LayerNorm(input_size),
+            nn.Linear(input_size, dim),
+            nn.LayerNorm(dim)
+        )
+
+        # ---- The fin token ---- #
+        self.fin_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.lp_dropout = nn.Dropout(lp_dropout)
+
+        # ---- The transformer encoder ---- #
+        self.transformer_encoder = Transformer(dim=dim, depth=depth, heads=heads, ff_hidden_dim=ff_hidden_dim, dropout=te_dropout)
+
+        # ---- The mlp head ---- #
+        self.mlp_head = nn.Linear(dim, 1)
+
+    def forward(self, feature: torch.Tensor) -> torch.Tensor:
         """ Forward function of FiT.
 
+        :param feature: the input feature of FiT, shape=(bs, n, input_size), `n` means
+            there are `n` samples of feature
 
-        :return:
-            - y_pred, shape=(bs, label_len, 1)
+        :return out: the output, shape=(bs, 1)
 
         """
 
-        print(self.device)
-        out = panel_features
+        # ---- Step 1. Do the LP ---- #
+        x = self.linear_projection(feature)  # shape=(bs, n, dim)
+        bs, n, _ = x.shape
+
+        # ---- Step 2. Concat the `fin_token` ---- #
+        fin_tokens = repeat(self.fin_token, "1 1 d -> bs 1 d", bs=bs)  # repeat (1, 1, dim) to (bs, 1, dim)
+        x = torch.cat((fin_tokens, x), dim=1)  # concat the token, shape = (bs, n+1, dim)
+        x = self.lp_dropout(x)  # do the drop out
+
+        # ---- Step 4. Do the TE ---- #
+        x = self.transformer_encoder(x)  # shape=(bs, n+1, dim)
+
+        # ---- Step 5. Do the summary ---- #
+        x = x.mean(dim=1) if self.pool == "mean" else x[:, 0]  # shape=(bs, dim)
+
+        # ---- Step 5. Do the MLP transformation ---- #
+        out = self.mlp_head(x)  # shape=(bs, 1)
         return out
--- Networks/tests/test_fit.py
+++ Networks/tests/test_fit.py
@@ -12,10 +12,26 @@ def test():
     # fix the seed of torch
     torch.manual_seed(0)
     # init the random feature
-    feature = torch.randn(64, 5, 768)
+    feature = torch.randn(64, 5, 768, dtype=torch.float32)
+    re_seq_feature = feature.clone()
+    re_seq_feature[:, 0, :] = feature[:, 4, :]
+    re_seq_feature[:, 4, :] = feature[:, 0, :]
+    re_seq_feature[:, 1, :] = feature[:, 3, :]
+    re_seq_feature[:, 3, :] = feature[:, 1, :]
+    assert not (torch.all(re_seq_feature == feature))
+
     # init the model
-    fit = FiT()
+    fit = FiT(
+        input_size=768,
+        dim=64,
+        depth=1,
+        heads=2,
+        ff_hidden_dim=32
+    )
+
     # forward
-    output = fit(feature)
+    output = fit(feature)  # shape=(64, 1)
+    re_seq_output = fit(re_seq_feature)  # shape=(64, 1)
+
     # check
-    assert output, f"Error !"
+    assert torch.all(torch.abs(output.detach() - re_seq_output.detach()) < 1e-6), f"Error !"
