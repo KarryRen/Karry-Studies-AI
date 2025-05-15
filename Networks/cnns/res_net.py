942a62f2ac52014aee5014816796c84229f76563
KarryRen <KarryRenKai@outlook.com>
2025-05-15 17:10:40 +0800

    finish: finish cnns (#11)

--- Networks/cnns/res_net.py
+++ Networks/cnns/res_net.py
@@ -64,6 +64,3 @@ class ResNet18(ResNet):
     def __init__(self, lr=0.1, num_classes=10):
         super().__init__(((2, 64), (2, 128), (2, 256), (2, 512)),
                          lr, num_classes)
-
-
-ResNet18().layer_summary((1, 1, 96, 96))
